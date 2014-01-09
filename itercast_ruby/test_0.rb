20e49226ad9692f333c90541202b41f3762a6a41
xu hao <ixuhao@gmail.com>
2014-01-09 17:06:44 +0800

    add itercast ruby lessons.

---
+++ itercast_ruby/test_0.rb
@@ -0,0 +1,22 @@
+# this is a comment
+10.times do
+=begin
+multi
+commments
+=end
+    puts "love"
+end
+
+time = 123142342
+_time = 123123123
+puts "hello #{time}"
+a = 'a'
+b = 'b'
+a, b = b, a
+#puts a, b
+3.times{puts "hello"}
+puts "hello" * 3
+
+if (time>5)
+    puts time
+end
