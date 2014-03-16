50a185b9c316060c86be59e613265583dcad8931
xu hao <ixuhao@gmail.com>
2014-03-16 16:34:40 +0800

    update mac's vimrc, rm smashing node

--- smashing_node_learning/chapter_1/hello_world.js
+++
@@ -1,5 +0,0 @@
-var http = require('http')
-var server = http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type':'text/html'});
-    response.end('<marquee>Smashing Node!</marquee>');
-}).listen(4000);
